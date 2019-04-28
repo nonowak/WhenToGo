92e68154c006526f3f74c957368be3f8989eb3a7
Norbert Nowak <no.nowak@outlook.com>
2019-04-28 23:13:15 +0200

    working hotel extraction but takes too much time TODO: getting hotels per city and then merge

---
+++ src/data/etl/config.py
@@ -0,0 +1,3 @@
+DEFAULT_DIRECTORY = "/Users/norbertnowak/Files/Projects/Home/WhenToGo"
+RAW_DATA_DIRECTORY = "{}/data/raw".format(DEFAULT_DIRECTORY)
+PROCESSED_DATA_DIRECTORY = "{}/data/interim".format(DEFAULT_DIRECTORY)
