a76bd1ac19b50a93ec0aa4f24554fc65f7ce784a
Norbert Nowak <no.nowak@outlook.com>
2019-06-24 20:58:52 +0200

    if dataframe is empty then avoid all process

--- src/data/etl-luigi/extractRawFiles.py
+++ src/data/etl-luigi/extractRawFiles.py
@@ -14,16 +14,17 @@ LUIGI_RAW_DIRECTORY = f'{conf.LUIGI_DIRECTORY}/raw'
 class ExtractRawFiles(luigi.Task):
     def run(self):
         for input_filename in glob.glob(f'{conf.RAW_DATA_DIRECTORY}/**/*.csv', recursive=True):
-            scrape_city = re.search('raw/(.*)/', input_filename).group(1)
-            scrape_date = input_filename[-20:-10]
-            scrape_hour = input_filename[-9:-7]
             df = pd.read_csv(input_filename, index_col=0)
-            df = df.assign(
-                **{'scrape_hour': scrape_hour,
-                   'scrape_date': scrape_date,
-                   'scrape_city': scrape_city}
-            )
-            cmn.create_directory(LUIGI_RAW_DIRECTORY)
-            scrape_directory = f'{LUIGI_RAW_DIRECTORY}/{scrape_city}_{scrape_date}_{scrape_hour}.csv'
-            df.to_csv(scrape_directory)
-            yield Load(raw_data_filename=scrape_directory)
+            if not df.empty:
+                scrape_date = input_filename[-20:-10]
+                scrape_hour = input_filename[-9:-7]
+                scrape_city = re.search('raw/(.*)/', input_filename).group(1)
+                df = df.assign(**{'scrape_hour': scrape_hour,
+                                  'scrape_date': scrape_date,
+                                  'scrape_city': scrape_city}
+
+                               )
+                cmn.create_directory(LUIGI_RAW_DIRECTORY)
+                scrape_directory = f'{LUIGI_RAW_DIRECTORY}/{scrape_city}_{scrape_date}_{scrape_hour}.csv'
+                df.to_csv(scrape_directory)
+                yield Load(raw_data_filename=scrape_directory)
